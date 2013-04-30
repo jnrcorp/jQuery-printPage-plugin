8cba59b7310ac72cee13f25aba36008e0dd3b0bf
jnrcorp <joshua.ross@gmail.com>
2013-04-29 21:00:45 -0300

    added support for URL Parameters and Chrome Print Override
    If you need to print from a URL that has parameters or is in Chrome, this should resolve issues with the original.

--- jquery.printPage.js
+++ jquery.printPage.js
@@ -1,82 +1,124 @@
 /**
+ * MODIFIED FROM ORIGINAL
  * jQuery printPage Plugin
+ * 
  * @version: 1.0
  * @author: Cedric Dugas, http://www.position-absolute.com
  * @licence: MIT
- * @desciption: jQuery page print plugin help you print your page in a better way
+ * @desciption: jQuery page print plugin help you print your page in a better
+ *              way
  */
+// detect if browser is Chrome
+if(navigator.userAgent.toLowerCase().indexOf("chrome") >  -1) {
+    // wrap private vars in a closure
+    (function() {
+        var realPrintFunc = window.print;
+        var interval = 2500; // 2.5 secs
+        var nextAvailableTime = +new Date(); // when we can safely print again
 
-(function( $ ){
+        // overwrite window.print function
+        window.print = function() {
+            var now = +new Date();
+            // if the next available time is in the past, print now
+            if(now > nextAvailableTime) {
+                realPrintFunc();
+                nextAvailableTime = now + interval;
+            } else {
+                // print when next available
+                setTimeout(realPrintFunc, nextAvailableTime - now);
+                nextAvailableTime += interval;
+            }
+        };
+    })();
+}
+(function($) {
   $.fn.printPage = function(options) {
-    // EXTEND options for this button
-    var pluginOptions = {
-      attr : "href",
-      url : false,
-      showMessage: true,
-      message: "Please wait while we create your document" 
-    };
-    $.extend(pluginOptions, options);
+		// EXTEND options for this button
+		var pluginOptions = {
+			attr : "href",
+			url : false,
+			message : "Please wait while we create your document",
+			paramForm: false
+		};
+		$.extend(pluginOptions, options);
+		this.die();
 
-    this.on("click", function(){  loadPrintDocument(this, pluginOptions); return false;  });
-    
-    /**
-     * Load & show message box, call iframe
-     * @param {jQuery} el - The button calling the plugin
-     * @param {Object} pluginOptions - options for this print button
-     */   
-    function loadPrintDocument(el, pluginOptions){
-      if(pluginOptions.showMessage){
-      $("body").append(components.messageBox(pluginOptions.message));
-      $("#printMessageBox").css("opacity", 0);
-      $("#printMessageBox").animate({opacity:1}, 300, function() { addIframeToPage(el, pluginOptions); });
-      } else {
-        addIframeToPage(el, pluginOptions);
-      }
-    }
-    /**
-     * Inject iframe into document and attempt to hide, it, can't use display:none
-     * You can't print if the element is not dsplayed
-     * @param {jQuery} el - The button calling the plugin
-     * @param {Object} pluginOptions - options for this print button
-     */
-    function addIframeToPage(el, pluginOptions){
+		this.live("click", function() {
+			loadPrintDocument(this, pluginOptions);
+			return false;
+		});
 
-        var url = (pluginOptions.url) ? pluginOptions.url : $(el).attr(pluginOptions.attr);
+		/**
+		 * Load & show message box, call iframe
+		 * 
+		 * @param {jQuery}
+		 *            el - The button calling the plugin
+		 * @param {Object}
+		 *            pluginOptions - options for this print button
+		 */
+		function loadPrintDocument(el, pluginOptions) {
+			$("body").append(components.messageBox(pluginOptions.message));
+			$("#printMessageBox").css("opacity", 0);
+			$("#printMessageBox").animate({
+				opacity : 1
+			}, 300, function() {
+				addIframeToPage(el, pluginOptions);
+			});
+		}
+		/**
+		 * Inject iframe into document and attempt to hide, it, can't use
+		 * display:none You can't print if the element is not dsplayed
+		 * 
+		 * @param {jQuery}
+		 *            el - The button calling the plugin
+		 * @param {Object}
+		 *            pluginOptions - options for this print button
+		 */
+		function addIframeToPage(el, pluginOptions) {
 
-        if(!$('#printPage')[0]){
-          $("body").append(components.iframe(url));
-          $('#printPage').on("load",function() {  printit(pluginOptions);  })
-        }else{
-          $('#printPage').attr("src", url);
-        }
-    }
-    /*
-     * Call the print browser functionnality, focus is needed for IE
-     */
-    function printit(){
-      frames["printPage"].focus();
-      frames["printPage"].print();
-      if(pluginOptions.showMessage){
-        unloadMessage();
-      }
-    }
-    /*
-     * Hide & Delete the message box with a small delay
-     */
-    function unloadMessage(){
-      $("#printMessageBox").delay(1000).animate({opacity:0}, 700, function(){
-        $(this).remove();
-      });
-    }
-    /*
-     * Build html compononents for thois plugin
-     */
-    var components = {
-      iframe: function(url){
-        return '<iframe id="printPage" name="printPage" src='+url+' style="display: none; @media print { display: block; }"></iframe>';
-      },
-      messageBox: function(message){
-        return "<div id='printMessageBox' style='\
+			var url = (pluginOptions.url) ? pluginOptions.url : $(el).attr(pluginOptions.attr),
+					urlParams = (pluginOptions.paramForm) ? "?"+pluginOptions.paramForm.serialize() : "";
+
+			url = url+urlParams;
+
+			if (!$('#printPage')[0]) {
+				$("body").append(components.iframe(url));
+				$('#printPage').bind("load", function() {
+					printit();
+				});
+			} else {
+				$('#printPage').attr("src", url);
+			}
+		}
+		/*
+		 * Call the print browser functionnality, focus is needed for IE
+		 */
+		function printit() {
+			frames["printPage"].focus();
+			frames["printPage"].print();
+			unloadMessage();
+		}
+		/*
+		 * Hide & Delete the message box with a small delay
+		 */
+		function unloadMessage() {
+			$("#printMessageBox").delay(1000).animate({
+				opacity : 0
+			}, 700, function() {
+				$(this).remove();
+			});
+		}
+		/*
+		 * Build html compononents for this plugin
+		 */
+		var components = {
+			iframe : function(url) {
+				return '<iframe id="printPage" name="printPage" src='
+						+ url
+						+ ' style="position:absolute;top:0px; left:0px;width:0px; height:0px;border:0px;overfow:none; z-index:-1"></iframe>';
+			},
+			messageBox : function(message) {
+				return "<div id='printMessageBox' style='\
           position:fixed;\
           top:50%; left:50%;\
           text-align:center;\
@@ -87,8 +129,9 @@
           border: 6px solid #555;\
           border-radius:8px; -webkit-border-radius:8px; -moz-border-radius:8px;\
           box-shadow:0px 0px 10px #888; -webkit-box-shadow:0px 0px 10px #888; -moz-box-shadow:0px 0px 10px #888'>\
-          "+message+"</div>";
-      }
-    }
-  };
-})( jQuery );
+          "
+						+ message + "</div>";
+			}
+		};
+	};
+})(jQuery);
